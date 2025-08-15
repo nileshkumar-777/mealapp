34f94d0d455e012e4289155c2e6aa085c2ccdc81
nileshkumar-777 <nilesh8kumar76@gmail.com>
2025-08-16 00:33:43 +0530

    Revert "Added meal details screen with image, ingredients, and recipe instruc…"

--- lib/widget/category_grid_items.dart
+++ lib/widget/category_grid_items.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mealapp/main.dart';
 import 'package:mealapp/model/category.dart';
 
 class CategoryGridItems extends StatelessWidget {
-  const CategoryGridItems({
+    const CategoryGridItems({
     super.key,
     required this.category,
     required this.onSelectCategory,
@@ -12,31 +13,33 @@ class CategoryGridItems extends StatelessWidget {
   final void Function() onSelectCategory;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return InkWell(
       onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
           gradient: LinearGradient(
             colors: [
               category.color.withOpacity(0.55),
               category.color.withOpacity(0.9),
+      
             ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+            begin:Alignment.topLeft,
+            end:Alignment.bottomRight,
+          )
         ),
         child: Text(
           category.title,
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-        ),
+          style:Theme.of(context).textTheme.titleLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          )
+          ),
       ),
     );
+
   }
 }
